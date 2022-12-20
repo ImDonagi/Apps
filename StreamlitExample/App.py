3b7be30b08c1c6c64baa8a13d811f4ff1a4bf8ad
Guy Donagi <107021187+ImDonagi@users.noreply.github.com>
2022-12-20 16:53:32 +0200

    Update App.py

--- StreamlitExample/App.py
+++ StreamlitExample/App.py
@@ -109,5 +109,5 @@ with segmented:
     
     area_col.subheader('Select The Desired Object:')
     area_col.text(centers)
-    area_col.text("Now that you have successfully segmented your image into the different objects it holds,\nselect the one whose area you want to calculate.\nEvery option in the select box is actually an RGB color that represents a segment in the image.\nIn order to understand which color is which, [use this](https://www.rapidtables.com/web/color/RGB_Color.html)")
+    area_col.text("Now that you have successfully segmented your image into the different objects it holds,\nselect the one whose area you want to calculate.\nEvery option in the\nselect box is actually an RGB color that represents a segment in the image.\nIn order to understand which color is which, [use this](https://www.rapidtables.com/web/color/RGB_Color.html)")
     obj = area_col.selectbox("", options=centers)
