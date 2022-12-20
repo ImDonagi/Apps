66caf0660859720cefc28fda9be04337616b42db
Guy Donagi <107021187+ImDonagi@users.noreply.github.com>
2022-12-20 14:13:30 +0200

    Update App.py

--- StreamlitExample/App.py
+++ StreamlitExample/App.py
@@ -1,44 +1,59 @@
-# import libs
+# imports
 import streamlit as st
 import cv2
 import numpy as np
 import skimage.io as io
 import matplotlib.pyplot as plt
 
-# vars
-DEMO_IMAGE = 'bananas.jpeg' # a demo image for the segmentation page, if none is uploaded
-favicon = 'favicon.png'
+# ----------------------------
 
-# main page
-st.set_page_config(page_title="Guy's app", page_icon = favicon, layout = 'wide', initial_sidebar_state = 'auto')
-st.title("Guy's App")
+# Functions:
+def segment_image_kmeans(img, k=3, attempts=10): 
 
-# side bar
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] . div:first-child{
-        width: 350px
-    }
+    # Convert MxNx3 image into Kx3 where K=MxN
+    pixel_values  = img.reshape((-1,3))  #-1 reshape means, in this case MxN
+
+    #We convert the unit8 values to float as it is a requirement of the k-means method of OpenCV
+    pixel_values = np.float32(pixel_values)
+
+    # define stopping criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
     
-    [data-testid="stSidebar"][aria-expanded="false"] . div:first-child{
-        width: 350px
-        margin-left: -350px
-    }    
-    </style>
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, attempts, cv2.KMEANS_RANDOM_CENTERS)
     
-    """,
-    unsafe_allow_html=True,
+    # convert back to 8 bit values
+    centers = np.uint8(centers)
+
+    # flatten the labels array
+    labels = labels.flatten()
+    
+    # convert all pixels to the color of the centroids
+    segmented_image = centers[labels.flatten()]
+    
+    # reshape back to the original image dimension
+    segmented_image = segmented_image.reshape(img.shape)
+    
+    return segmented_image
+# ----------------------------
+
+# Interface:
+
+header = st.container()
+input = st.container()
+output = st.container()
+
+with header:
+    st.title("Area Calculating Using AruCo Markers")
+    st.text("This small project was built as a part of \"Intro To Image Processing\"\ncourse in the Faculty of Agriculture.\nIt's quite simple:\n")
+    st.text("*  Upload an image containing an object and an AruCo marker to the\n \"Input Image\" section.")
+    st.text("*  The area of the object will be presented in the \"Calculated Area\" section.")
+
+with input:
+    st.header("Input Values:")
+
+    sel_col = st.container()
+    disp_col = st.container()
 
 
-)
-
-st.sidebar.title('Segmentation Sidebar')
-st.sidebar.subheader('Site Pages')
-
-st.markdown(
-    """
-    <h1>hello</h1>
-    """
-)
-
+with output:
+    st.header("Calculated Area:")
