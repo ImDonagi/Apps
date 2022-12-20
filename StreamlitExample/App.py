d39b898f89dc1eeb86537e289f66f72601b02fa3
Guy Donagi <107021187+ImDonagi@users.noreply.github.com>
2022-12-20 14:32:35 +0200

    Update App.py

--- StreamlitExample/App.py
+++ StreamlitExample/App.py
@@ -75,13 +75,14 @@ with header:
     st.text("*  The area of the object will be presented in the \"Calculated Area\" section.")
 
 with input:
-    st.header("Input Values:")
-
     sel_col, disp_col = st.columns(2)
     
+    sel_col.subheader("Input Values:")
+    img_file = st.sidebar.file_uploader("Please upload an image", type=['jpg', 'jpeg', 'png'])
     k = sel_col.slider('Please choose number of clusters:', value=3, min_value=2, max_value=10)
     attempts = sel_col.slider('Please choose number of attempts:', value=7, min_value=1, max_value=10)
     
-    disp_col.subheader("ayo")
+    disp_col.subheader("ayo:")
+    
 with output:
     st.header("Calculated Area:")
